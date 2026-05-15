bacd6825bfc84ec94f61a90bf95383d964256b09
Amit KB <mr.amitkb@gmail.com>
2026-05-15 20:32:24 +0600

    Initial setup for ScholarStream client (A11)

---
+++ src/pages/Home/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            ScholerStream MERN Project starts...
+        </div>
+    );
+};
+
+export default Homepage;
